4e3a12149dd7f4c464e4a9f1237ccbb0d6ac2773
gwanga <wangageremi725@gmail.com>
2026-02-25 14:37:52 +0300

    food_prices

--- main.go
+++ main.go
@@ -1,13 +1,28 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
 
 func main() {
-  router := gin.Default()
-  router.GET("/ping", func(c *gin.Context) {
-    c.JSON(200, gin.H{
-      "message": "pong",
-    })
-  })
-  router.Run() // listens on 0.0.0.0:8080 by default
+	// Alternative 1: FAOSTAT Food Security Indicators
+	// url1 := "https://fenixservices.fao.org/faostat/api/v1/en/data/FS"
+	
+	// // Alternative 2: World Bank API for prices (often more reliable)
+	url2 := "https://api.worldbank.org/v2/country/KE/indicator/FP.CPI.TOTL?format=json"
+	
+	// Alternative 3: FAO FPMA Tool (different endpoint)
+	// url3 := "http://www.fao.org/giews/food-prices/home/api/en/"
+	
+	resp, err := http.Get(url2)
+	if err != nil {
+		log.Fatal("API Error:", err)
+	}
+	defer resp.Body.Close()
+	
+	body, _ := io.ReadAll(resp.Body)
+	fmt.Println(string(body))
 }
