0b6cf20b04737b862c725f665bdd047a0b89b2f3
KwachOjunga <ojungakwach@gmail.com>
2026-02-25 16:05:07 +0300

    define structs to contain data

--- main.go
+++ main.go
@@ -1,28 +1,76 @@
 package main
 
 import (
-	"fmt"
-	"io"
-	"log"
-	"net/http"
+	"github.com/gin-gonic/gin"
 )
 
+type PriceType int
+
+const (
+	WholeSale PriceType = iota
+	Retail
+)
+
+type Currency int
+
+const (
+	KEs PriceType = iota
+	USD
+)
+
+type PriceFlag int
+
+const (
+	actual PriceFlag = iota
+	aggregate
+	composite //both actual and aggregate
+)
+
+type FoodCategory struct {
+	Name  string `json:"name"`
+	Foods []Commodity `json:"foods"`
+}
+
+type Commodity struct {
+	Id        int `json:"id"`
+	Name      string `json:"name"`
+	Currency  Currency `json:"currency"`
+	PriceFlag PriceFlag `json:"price_flag"`
+	PriceType PriceType `json:"price_type"`
+	Date      string `json:"date"`
+}
+
+type Location struct {
+	Lat  int `json:"lat"`
+	Long int `json:"long"`
+}
+
+type MarketData struct {
+	Name           string `json:"name"`
+	Location       Location `json:"location"`
+	FoodCategories []FoodCategory `json:"food_categories"`
+}
+
+type FoodData struct {
+	Markets        []MarketData `json:"markets"`
+}
+
+var data_file string = "./wfp_food_prices_ken.csv"
+
+func ReadData(file string) FoodData {
+	// open file
+	// parse file -> and populate structs before sending to frontend
+	return FoodData{}
+}
+
+
+
 func main() {
-	// Alternative 1: FAOSTAT Food Security Indicators
-	// url1 := "https://fenixservices.fao.org/faostat/api/v1/en/data/FS"
-	
-	// // Alternative 2: World Bank API for prices (often more reliable)
-	url2 := "https://api.worldbank.org/v2/country/KE/indicator/FP.CPI.TOTL?format=json"
-	
-	// Alternative 3: FAO FPMA Tool (different endpoint)
-	// url3 := "http://www.fao.org/giews/food-prices/home/api/en/"
-	
-	resp, err := http.Get(url2)
-	if err != nil {
-		log.Fatal("API Error:", err)
-	}
-	defer resp.Body.Close()
-	
-	body, _ := io.ReadAll(resp.Body)
-	fmt.Println(string(body))
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	router.Run() // listens on 0.0.0.0:8080 by default
 }
